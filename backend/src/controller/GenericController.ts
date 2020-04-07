41408e0f03ff15cef78fe42253a669c3b43f132c
SupNL <gustavoonishinl@gmail.com>
2020-04-07 12:49:07 -0300

    Removendo imports não usados e seguindo padrão TS

--- backend/src/controller/GenericController.ts
+++ backend/src/controller/GenericController.ts
@@ -1,5 +1,5 @@
-import { Connection, createConnection ,Repository, getConnection, ObjectID, DeleteResult, ObjectType, QueryFailedError } from "typeorm";
-
+import { Repository, getConnection, DeleteResult, ObjectID, ObjectType } from "typeorm";
+import { Request } from 'express';
 
 export default class UserController<T> {
     private repository: Repository<T>;
@@ -10,7 +10,7 @@ export default class UserController<T> {
         this.repository = getConnection().getRepository(this.classType);
     }
 
-    public processBody(body): T | undefined {
+    public processBody(body : Request["body"]): T | undefined {
         throw Error(`PROCESS BODY NOT IMPLEMENTED IN ${this.classType}`);
     }
 
@@ -18,31 +18,31 @@ export default class UserController<T> {
         return this.repository.find();
     }
 
-    public async getByPk(id?: number | string | ObjectID): Promise<T> {
+    public async getByPk(id : number | string | ObjectID): Promise<T> {
         return this.repository.findOne(id);
     }
 
-    public async create(body): Promise<T[]> {
+    public async create(body : Request["body"]): Promise<T[]> {
         const object: T = this.processBody(body);
 
         if (object) {
             return this.repository.save([object]);
         } 
-        throw Error(`Error in the atributes from User`);
+        throw Error(`Error in the attributes from ${this.classType}`);
     }
 
-    public async edit(body, targetId): Promise<T> {
+    public async edit(body : Request["body"], id : number | string | ObjectID): Promise<T> {
         const object: T = this.processBody(body);
 
         if (object) {
-            const user = await this.repository.findOne(targetId);
+            const user = await this.repository.findOne(id);
             this.repository.merge(user, object);
             return this.repository.save(user);
         }
-        throw Error(`Error in the atributes from User`);
+        throw Error(`Error in the attributes from ${this.classType}`);
     }
 
-    public async delete(id): Promise<DeleteResult> {
+    public async delete(id : number | string | ObjectID): Promise<DeleteResult> {
         return this.repository.delete(id);
     }
 

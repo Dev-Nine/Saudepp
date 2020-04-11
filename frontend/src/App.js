783e71e24a8bc05763053bf637f9ba8ac2f217b3
Leandro Souza <leandror.dev@gmail.com>
2020-04-11 14:46:23 -0300

    Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
+import Routes from './routes'
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;

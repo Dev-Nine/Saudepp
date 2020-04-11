4d5a2180b7b41707d34f28e010d1d60685544fb0
Leandro Souza <leandror.dev@gmail.com>
2020-04-11 17:18:33 -0300

    Card component. 04/11/2020

---
+++ frontend/src/components/Card/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './styles.css'
+
+import Notice from '../Notice'
+
+export default function Card({title, type, data}) {
+   return (
+      <>
+         <h1>{title}</h1>
+         <hr />
+         <ul>
+            <li><Notice /></li>
+            <li><Notice /></li>
+            <li><Notice /></li>
+            <li><Notice /></li>
+         </ul>
+      </>
+   )
+}
--- frontend/src/screens/Home/index.js
+++ frontend/src/screens/Home/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import './styles.css'
 
-import Notice from '../../components/Notice'
+import Card from '../../components/Card'
 
 export default function Home() {
    return (
       <div id='main'>
-         a
-
+         <Card title='Novas noticias' data='' type='new' />
+         <Card title='Recomendações' data='' type='rec' />
+         <Card title='Saúde e bem estar' data='' type='health' />
       </div>
    )
 }

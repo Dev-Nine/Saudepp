e58e76bc688f4a01f15d676164180c154ceb9470
Your Name <you@example.com>
2020-04-11 22:23:21 -0300

    Login

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './screens/Home';
+import Login from './screens/Login';
 
 export default function Routes() {
    return (
       <BrowserRouter>
          <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/Login" exact component={Login}/>
          </Switch>
       </BrowserRouter>
    )

d6ee8745480bf20b7d29c7baa30d92bbe9a620d2
SupNL <gustavoonishinl@gmail.com>
2020-04-15 14:50:53 -0300

    Create e edit com hash na senha

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -1,6 +1,7 @@
 import { User } from '../model/User';
 import { Request, Response } from 'express';
 import GenericController from './GenericController';
+import { hash } from 'bcryptjs';
 
 export default class UserController extends GenericController<User> {
     constructor() {
@@ -49,5 +50,49 @@ export default class UserController extends GenericController<User> {
         user.password = body.password;
 
         return user; 
+    }
+
+    public async create(req : Request, res : Response) : Promise<Response>{
+        try{
+            const statusCode = this.validateCreate(req);
+
+            if(statusCode != 200)
+                return res.status(statusCode).send();
+
+            req["body"].password = await hash(req["body"].password, 8);
+
+            const user: User = this.processCompleteData(req);
+
+            if (user) {
+                const result: User[] = await this.repository.save([user]);
+                return res.json(result);
+            } 
+
+            throw Error(`Error in the attributes from ${this.classType}`);
+        }catch(err){
+            return this.validateError(err, res);
+        }
+    }
+
+    public async edit(req : Request, res : Response): Promise<Response> {
+        try{
+            const statusCode = this.validateEdit(req);
+            if(statusCode != 200)
+                return res.status(statusCode).send();
+
+            req["body"].password = await hash(req["body"].password, 8);
+
+            const user: User = this.processData(req);
+
+            if (user) {
+                const foundUser = await this.repository.findOne(req.params["id"]);
+                this.repository.merge(foundUser, user);
+                const result = await this.repository.save(foundUser);
+                return res.json(result);
+            }
+            throw Error(`Error in the attributes from ${this.classType}`);
+        }catch(err){
+            return this.validateError(err, res);
+        }
     }
 }

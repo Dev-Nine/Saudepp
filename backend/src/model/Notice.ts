9b1320d46da185eec27da34b5401b31ab1f340f6
SupNL <gustavoonishinl@gmail.com>
2020-04-15 13:35:07 -0300

    Cascade ao deletar notícia (sujeito a mudança)

--- backend/src/model/Notice.ts
+++ backend/src/model/Notice.ts
@@ -1,6 +1,7 @@
-import {Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, ManyToMany, JoinColumn} from "typeorm";
 import {User} from "./User";
 import {Comment} from "./Comment";
+import { Tag } from "./Tag";
 
 @Entity()
 export class Notice {
@@ -24,9 +25,14 @@ export class Notice {
     comments: Comment[];
 
     @ManyToOne(type => User, user => user.notices, {
-        eager: true // carregar dados da foreign key
+        eager: true, // carregar dados da foreign key
+        onDelete: "CASCADE"
     })
     user: User;
+
+    @ManyToMany(type => Tag)
+    @JoinColumn()
+    tags: Tag[]
 
     public isValid(): boolean {
         if (this.title && this.date && this.text && this.user) {

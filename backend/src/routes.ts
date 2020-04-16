5f9995d72426798fc731fd8697261c078a1b745c
SupNL <gustavoonishinl@gmail.com>
2020-04-16 12:17:31 -0300

    add: routes for tag

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,12 +7,16 @@ import CommentController from "./controller/CommentController";
 import SessionController from "./controller/SessionController";
 
 import ensureAuthentication from './middlewares/ensureAuthenticated'
+import TagController from "./controller/TagController";
+import SubTagController from "./controller/SubTagController";
 
 export default class Routes {
     public routes: Router;
     private userController: UserController;
     private noticeController: NoticeController;
     private commentController: CommentController;
+    private tagController: TagController;
+    private subTagController: SubTagController;
     private sessionController: SessionController;
 
     constructor() {
@@ -20,29 +24,20 @@ export default class Routes {
         this.userController = new UserController();
         this.noticeController = new NoticeController();
         this.commentController = new CommentController();
+        this.tagController = new TagController();
+        this.subTagController = new SubTagController();
         this.sessionController = new SessionController();
     }
 
     public defineRoutes() {
         // USUARIOS
-
-        //Return all users
         this.routes.get("/users", this.userController.getAll.bind(this.userController));
-
-        //Return user by id
         this.routes.get("/users/:id", this.userController.getByPk.bind(this.userController));
-
-        //Creating a user
         this.routes.post("/users", this.userController.create.bind(this.userController));
-
-        //Editing a user
         this.routes.put("/users/:id", this.userController.edit.bind(this.userController));
-
-        //Deleting a user
         this.routes.delete("/users/:id", this.userController.delete.bind(this.userController));
 
         // NOTICIAS
-
         this.routes.get("/notices", this.noticeController.getAll.bind(this.noticeController));
         this.routes.get("/notices/:id",this.noticeController.getByPk.bind(this.noticeController));
         this.routes.post("/notices", this.noticeController.create.bind(this.noticeController));
@@ -50,15 +45,21 @@ export default class Routes {
         this.routes.delete("/notices/:id", this.noticeController.delete.bind(this.noticeController));
 
         // COMENTARIOS
-
         this.routes.get("/comments", this.commentController.getAll.bind(this.commentController));
         this.routes.get("/comments/:id", this.commentController.getByPk.bind(this.commentController));
         this.routes.post("/comments", this.commentController.create.bind(this.commentController));
         this.routes.put("/comments/:id", this.commentController.edit.bind(this.commentController));
         this.routes.delete("/comments/:id", this.commentController.delete.bind(this.commentController));
 
+        // TAG
+        this.routes.get("/tags", this.tagController.getAll.bind(this.tagController));
+        this.routes.get("/tags/:id", this.tagController.getByPk.bind(this.tagController));
+        this.routes.post("/tags", this.tagController.create.bind(this.tagController));
+        this.routes.put("/tags/:id", this.tagController.edit.bind(this.tagController));
+        this.routes.delete("/tags/:id", this.tagController.delete.bind(this.tagController));
+
         // SESSÕES (LOGIN)
-        this.routes.post("/sessions", this.sessionController.auth.bind(this.sessionController) )
+        this.routes.post("/sessions", this.sessionController.auth.bind(this.sessionController));
         
     }
 }

8e4c9a8f754b83768dd1934f60b358042edeaab3
SupNL <gustavoonishinl@gmail.com>
2020-04-29 20:34:46 -0300

    same as ensure, but only verifies

---
+++ backend/src/middlewares/verifyAuthentication.ts
@@ -0,0 +1,43 @@
+import { getConnection } from 'typeorm';
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+import { User } from '../model/User';
+
+interface tokenPayload {
+   iat: number;
+   exp: number;
+   sub: string;
+}
+
+export default function ensureAuthentication(req: Request, res: Response, next: NextFunction):void {
+   const authHeader = req.headers.authorization;
+
+   try {
+      if (!authHeader)
+         return next();
+   
+      const [, token] = authHeader.split(' ');
+      
+      const decoded = verify(token, authConfig.jwt.secret);
+
+      const {sub} = decoded as tokenPayload;
+
+      getConnection().getRepository(User).findOne(sub)
+         .then(function(user : User){
+         req.user = {
+            id : sub,
+            type: user.type
+         };
+
+         //console.log(decoded);
+         return next();
+      });
+      
+   } catch(err) {
+      // http 403 = forbidden
+      console.log(err.message);
+      res.sendStatus(403);
+   }
+}

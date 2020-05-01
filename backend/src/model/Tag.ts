cf0080e5e9912465417ab5b9c97f72c94279b464
SupNL <gustavoonishinl@gmail.com>
2020-05-01 12:42:27 -0300

    add: attr group, check if it groups subtags

--- backend/src/model/Tag.ts
+++ backend/src/model/Tag.ts
@@ -13,6 +13,11 @@ export class Tag {
     @MinLength(2)
     description: string;
 
+    @Column()
+    group: boolean;
+    // se group == true, nao pode ser utilizada como tag real
+    // ao inves disso, suas subtags sao quem serao utilizadas
+
     @OneToMany(type => SubTag, subTag => subTag.tag, {
         eager: true
     })

b29b96586a79f16f3202c8236a5b66d4af7f61fa
SupNL <gustavoonishinl@gmail.com>
2020-06-01 11:10:48 -0300

    change: corona virus info is now a component

--- frontend/src/screens/Home/index.js
+++ frontend/src/screens/Home/index.js
@@ -1,63 +1,19 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 
 import './styles.css'
 
 import Card from '../../components/Card'
 import Header from '../../components/Header';
-import api from '../../utils/api';
-
-import image from '../../image/coronavirus-4833754_1920-1230x450.jpg'
+import CoronaCard from '../../components/CoronaCard'
 
 export default function Home() {
-   const [covid, setCovid] = useState({})
-
-   useEffect( () => {
-      async function loadCovidData() {
-         const { data } = await api.get('/covid')
-         setCovid({
-            contagion: data.contagion.toLocaleString('pt'),
-            deaths: data.deaths.toLocaleString('pt')
-         }); 
-      }
-
-      loadCovidData()
-   }, [])
 
    return (
       <>
          <Header />
+
          <div id='main'>
-            <div id='corona_container' style={{borderRadius: 8, backgroundImage: `url(${image})`, width: '100%', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}}>                  
-               <div id='corona_info'>
-                  <div style={{marginLeft: 41, marginTop: 57}}>
-                     <p style={{fontSize: 55, marginBottom: 10}}>
-                        CORONA VIRUS
-                     </p>
-                     <p>
-                        Fique em <strong>CASA</strong>
-                        <br/>
-                        Se <strong>PREVINA</strong>
-                     </p>
-                  </div>
-                  <div style={{marginTop: 85, marginLeft: '30%',display: 'grid', height: 66,gridTemplateRows: '50% 50%'}}>
-                     <p style={{display: 'grid', gridTemplateColumns: '30% 70%'}}>
-                        <strong>
-                           {covid.contagion}
-                        </strong> 
-                        Casos confirmados
-                     </p>
-                     <p style={{display: 'grid', gridTemplateColumns: '30% 70%'}}>
-                        <strong style={{color: 'red'}}>
-                        {covid.deaths} 
-                        </strong>
-                        
-                        Óbitos confirmados
-                     </p>
-                  </div>
-
-               </div>
-            </div>
-
+            <CoronaCard />
             <div id="sobre">
                <p>
                   A plataforma digital denominada Saúde++, visa atualizar e orientar os cidadãos 

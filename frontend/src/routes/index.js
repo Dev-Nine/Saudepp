0164a4cafe360acf18181fca4890a9c0c7366f4b
SupNL <gustavoonishinl@gmail.com>
2020-06-01 12:36:44 -0300

    change: add NoticeDisplay route

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -6,8 +6,9 @@ import Home from '../screens/Home';
 import Login from '../screens/Login';
 import Noticies from '../screens/Noticies';
 import Register from '../screens/register';
-import RegisterProfessional from '../screens/RegisterProfessional'
+import RegisterProfessional from '../screens/RegisterProfessional';
 import RegisterMedic from '../screens/RegisterMedic';
+import NoticeDisplay from '../screens/NoticeDisplay'; 
 
 
 export default function Routes() {
@@ -16,6 +17,7 @@ export default function Routes() {
             <Route path="/" exact component={Home} />
             <Route path="/login" exact component={Login}/>
             <Route path="/noticies" exact component={Noticies}/>
+            <Route path="/notices/:noticeId" exact component={NoticeDisplay}/>
             <Route path="/register" exact component={Register}/> 
             <Route path="/register/professional" isPrivate exact component={RegisterProfessional}/>
             <Route pasth="/register/medic" exact component={RegisterMedic}/>

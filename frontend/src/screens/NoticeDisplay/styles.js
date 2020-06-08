7ad231ea17768536d4862daec19aa58fa33f6af5
SupNL <gustavoonishinl@gmail.com>
2020-06-08 15:24:51 -0300

    update: styles

--- frontend/src/screens/NoticeDisplay/styles.js
+++ frontend/src/screens/NoticeDisplay/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const ContainerNoticia = styled.div`
     background-color: #F9F7F7;
-    padding: 0 40px;
+    padding: 5px 40px;
     border-radius: 10px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
@@ -10,19 +10,61 @@ export const ContainerNoticia = styled.div`
         margin-bottom: 15px;
     }
 
-    p {
+    p:nth-child(2) {
         font-weight: 400;
         font-style: italic;
-    }
-
-    p + p {
-        font-style: normal;
+        color: #4A4A4A;
     }
 `
 
 export const ContainerComentario = styled.div`
     background-color: #F9F7F7;
-    padding: 0 40px;
+    padding: 20px 40px;
     border-radius: 10px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`
+
+export const EscreverComentario = styled.div`
+    background-color: #FFF;
+    padding: 20px 50px;
+    border-radius: 10px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+
+    textarea { 
+        margin-top: 20px;
+        box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+        border-radius: 8px;
+        width: 100%;
+        height: 160px;
+        padding: 15px;
+
+        border: none;
+        resize: none;
+        outline: none;
+    }
+
+    button{
+        width: 120px;
+        height: 35px;
+        background: #77C6FF;
+        font-weight: 500;
+        color: white;
+        align-self: flex-end;
+        border-radius: 8px;
+        margin-top: 10px;
+    }
+`
+
+export const MarkdownContainer = styled.div`
+    ol, ul {
+        list-style-position: inside;
+    }
+
+    li {
+        background-color: inherit;
+        display: list-item;
+    }
 `

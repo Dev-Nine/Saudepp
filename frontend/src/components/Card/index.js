360b24b74e243ebac77c21cc95063d59fa14412e
SupNL <gustavoonishinl@gmail.com>
2020-06-08 20:07:12 -0300

    fix: classnames for component card

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -7,13 +7,13 @@ import Notice from '../Notice'
 export default function Card({title, type, data}) {
    return (
       <div>
-         <h1>{title}</h1>
-         <hr />
-         <ul>
-            <li><Notice /></li>
-            <li><Notice /></li>
-            <li><Notice /></li>
-            <li><Notice /></li>
+         <h1 className="card-title">{title}</h1>
+         <hr className="card-hr" />
+         <ul className="card-list">
+            <li className="card-item"><Notice /></li>
+            <li className="card-item"><Notice /></li>
+            <li className="card-item"><Notice /></li>
+            <li className="card-item"><Notice /></li>
          </ul>
       </div>
    )

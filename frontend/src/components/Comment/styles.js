0f8ebb08c396d5045335ed9f43d2843c2f182fb8
SupNL <gustavoonishinl@gmail.com>
2020-06-08 15:23:53 -0300

    change: comment style

--- frontend/src/components/Comment/styles.js
+++ frontend/src/components/Comment/styles.js
@@ -2,15 +2,16 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     background-color: #FFF;
-    padding: 0 50px;
+    padding: 10px 50px;
     border-radius: 10px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    margin-top: 10px;
 
     div {
         display: flex;
         width: auto;
         align-items: center;
-        margin-top: 10px;
+        padding-top: 10px;
     }
 
     p:nth-child(3){
@@ -24,6 +25,7 @@ export const Container = styled.div`
     div:nth-child(2) {
         box-shadow: inset 0px 4px 2px rgba(0, 0, 0, 0.25);
         border-radius: 8px;
+        margin-top: 10px;
         padding: 15px 15px;
     }
 

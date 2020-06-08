ff8f9f08e3e84e02855c1b64a7cf150d59290a1d
SupNL <gustavoonishinl@gmail.com>
2020-06-08 15:23:43 -0300

    change: comment now changes based on props

--- frontend/src/components/Comment/index.js
+++ frontend/src/components/Comment/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Container } from './styles'
 
 export default function Comment(props) {
-
+    const { name, content } = props;
     return (
         <Container>
             <div>
@@ -11,12 +11,12 @@ export default function Comment(props) {
                     src="https://avatars1.githubusercontent.com/u/44099109?s=460&v=4" 
                     alt="Avatar"
                 />
-                <h3>nome</h3>
+                <h3>{name}</h3>
                 <p>1 de abril de 2020, 13:51</p>
             </div>
             
             <div>
-                <p>Somente o alcool em gel é recomendado?</p>
+                <p>{content}</p>
             </div>
         </Container>
     )

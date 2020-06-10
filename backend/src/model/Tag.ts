291ed401804cc3de6939a59dce7d876c3a6a8500
SupNL <gustavoonishinl@gmail.com>
2020-06-10 13:53:03 -0300

    remove: subtag and group column

--- backend/src/model/Tag.ts
+++ backend/src/model/Tag.ts
@@ -1,6 +1,5 @@
-import {Entity, Column, PrimaryColumn, OneToMany} from "typeorm";
+import {Entity, Column, PrimaryColumn} from "typeorm";
 import { IsString, MinLength } from 'class-validator';
-import {SubTag} from "./SubTag";
 
 @Entity()
 export class Tag {
@@ -12,15 +11,4 @@ export class Tag {
     @IsString()
     @MinLength(2)
     description: string;
-
-    @Column()
-    group: boolean;
-    // se group == true, nao pode ser utilizada como tag real
-    // ao inves disso, suas subtags sao quem serao utilizadas
-
-    @OneToMany(type => SubTag, subTag => subTag.tag, {
-        eager: true
-    })
-    subTags: SubTag[];
-
 }

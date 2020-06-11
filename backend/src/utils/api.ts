7062ae390737dfc2e80906eb620a0119744cf283
SupNL <gustavoonishinl@gmail.com>
2020-06-11 12:13:10 -0300

    change: new api

--- backend/src/utils/api.ts
+++ backend/src/utils/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'hhttps://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/',
+    baseURL: 'https://covid19-brazil-api.now.sh/',
 });

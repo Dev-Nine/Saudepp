4981738efb3d10c7980af60effe9b14043a0dc5e
SupNL <gustavoonishinl@gmail.com>
2020-06-22 12:42:40 -0300

    change: sobre now fills screen

--- frontend/src/screens/Home/styles.js
+++ frontend/src/screens/Home/styles.js
@@ -1,33 +1,45 @@
 import styled from 'styled-components'
 
 export const Sobre = styled.div`
-   position: relative;
-   display: block;
-   width: 1210px;
-   height: 220px;
-   border: black;
+   display: flex;
+   flex-direction: column;
+   height: auto;
    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
    border-radius: 8px;
    margin-bottom: 38px;
+   padding: 24px 39px;
 
    p {
-      padding-top: 24px;
-      width: 1132px;
       color: black;
       font-style: italic;
-      font-size: 30px;
+      font-size: 28px;
       font-weight: 500;
-      line-height: 35px;
       text-align: justify;
-      padding-left: 39px;
+      padding-right: 0.1em;
    }
 
    a {
-      position: relative;
-      padding-left: 85%;
+      margin-top: 8px;
       font-style: normal;
       font-weight: bold;
-      font-size: 25px;
+      align-self: flex-end;
+      font-size: 24px;
       text-align: justify;
+   }
+
+   @media only screen and (max-width: 1099px){
+      padding: 24px 20px;
+
+      p, a {
+         font-size: 16px;
+      }
+
+      p {
+         overflow: hidden;
+         text-overflow: ellipsis;
+         display: -webkit-box;
+         -webkit-line-clamp: 7; /* number of lines to show */
+         -webkit-box-orient: vertical;
+      }
    }
 `

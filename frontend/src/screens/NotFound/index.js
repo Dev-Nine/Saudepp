0a5e1a9386ce8ceff0317549d17a1254b3851198
phaalonso <phaalonso@gmail.com>
2020-06-22 10:30:35 -0300

    Changes

---
+++ frontend/src/screens/NotFound/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+export default function NotFound() {
+    return (
+	<>
+	    <Header />
+	    <h1> Page don't found </h1>
+	    <Footer />
+	</>
+    );
+}

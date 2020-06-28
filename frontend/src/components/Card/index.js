83ff7e01c23a7958747dca80ecc6eb5a11996669
SupNL <gustavoonishinl@gmail.com>
2020-06-27 22:53:09 -0300

    update: display formatted date

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -16,6 +16,29 @@ export default function Card({ data }) {
 	    }
     }    
 
+    function showTime(date){
+        date = new Date(date);
+        const currentDate = new Date();
+        const diff = currentDate.getTime() - date.getTime();
+        if(diff < 60000) // less than 1 minute
+            return "Publicado agora!"
+        if(diff < 3600000){ // less than 1 hour
+            var mins = parseInt(diff / 60000);
+            if(mins === 1)
+                return `Publicado a 1 minuto`
+            return `Publicado a ${mins} minutos`
+        }
+        if(diff < 86400000){ // less than a day
+            var hours = parseInt(diff / 3600000);
+            if(hours === 1)
+                return `Publicado a 1 hora`
+            return `Publicado a ${hours} horas`
+        }else{
+            return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}, 
+                às ${date.getHours()}:${("0" + String(date.getMinutes())).slice(-2)}`
+        }
+    }
+
    return (
       
 	<Link to={`/notices/${data.id}`} style={{ textDecoration: 'none', color: 'black' }}>
@@ -26,7 +49,7 @@ export default function Card({ data }) {
                 <p>
                 { data.abstract }
                 </p>
-                <span> { data.date } </span>
+                <span> { showTime(data.date) } </span>
                 <span> { data.user.name } </span>
             </div>
         </ Container>

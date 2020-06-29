7d629ad5d3674270c7b0654f76adc45bfd36a8f7
SupNL <gustavoonishinl@gmail.com>
2020-06-29 13:34:07 -0300

    fix: make width fill space

--- frontend/src/screens/NoticeDisplay/styles.js
+++ frontend/src/screens/NoticeDisplay/styles.js
@@ -6,6 +6,8 @@ export const ContainerNoticia = styled.div`
     border-radius: 10px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     margin-bottom: 20px;
+    width: 100%;
+    max-width:1300px;
 
     h1, p {
         margin-bottom: 15px;

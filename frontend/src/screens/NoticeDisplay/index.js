25fda7dd447c89dd828ecb112e47b3fc43ab2452
SupNL <gustavoonishinl@gmail.com>
2020-06-29 11:26:11 -0300

    under progress

--- frontend/src/screens/NoticeDisplay/index.js
+++ frontend/src/screens/NoticeDisplay/index.js
@@ -8,19 +8,70 @@ import CoronaCard from '../../components/CoronaCard'
 //import Comment from '../../components/Comment'
 import Footer from '../../components/Footer';
 
+import api from '../../utils/api';
+
 import { ContainerNoticia, ContainerComentario, EscreverComentario, TextContainer } from './styles'
 
-export default function NoticeDisplay() {
+export default function NoticeDisplay(props) {
 
-    const [content, setContent] = useState('');
+    const [content, setContent] = useState({
+        title: "Como lavar as mãos",
+        abstract: "Nessa epóca de pandemia, é extramente necessário lavar as mãos!",
+        text: "",
+        user: {
+            name: "Leandro Ribeiro"
+        }
+    });
+    
+    function getMonthAsName(month) {
+        switch(month){
+            default:
+                return "";
+            case 1:
+                return "janeiro";
+            case 2:
+                return "fevereiro";
+            case 3:
+                return "março";
+            case 4:
+                return "abril";
+            case 5:
+                return "maio";
+            case 6:
+                return "junho";
+            case 7:
+                return "julho";
+            case 8:
+                return "agosto";
+            case 9:
+                return "setembro";
+            case 10:
+                return "outubro";
+            case 11:
+                return "novembro";
+            case 12:
+                return "dezembro";
+        }
+    }
 
     useEffect(() => {
-        fetch(data)
-        .then(res => res.text())
-        .then(html => {
-            setContent(html);
-        })
-    }, []);
+        const { noticeId } = props.match.params.noticeId;
+        console.log(noticeId);
+        if(noticeId === undefined){
+            fetch(data)
+            .then(res => res.text())
+            .then(html => {
+                setContent({
+                    ...content,
+                    text: html
+                });
+            })
+        }else{
+            api.get(`/notices/${noticeId}`).then(({ data }) => {
+                
+            }); 
+        }
+    }, [content, props.match.params, props.match.params.id]);
 
     return(
         <>
@@ -29,11 +80,11 @@ export default function NoticeDisplay() {
                 <CoronaCard />
                 <ContainerNoticia>
                     <div>
-                        <h1>Como lavar as mãos</h1>
-                        <p>Nessa epóca de pandemia, é extramente necessário lavar as mãos!</p>
-                        <p>Escrito por Leandro R. - 1 de abril de 2020,</p>
+                        <h1>{content.title}</h1>
+                        <p>{content.abstract}</p>
+                        <p>Escrito por {content.user.name} - 1 de abril de 2020</p>
                         <TextContainer>
-                            {parse(content)}
+                            {parse(content.text)}
                         </TextContainer>
                     </div>
                 </ ContainerNoticia>

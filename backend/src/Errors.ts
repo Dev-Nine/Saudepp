e999c2a6d2634fe57b22cadac0c788e8f40e736d
SupNL <gustavoonishinl@gmail.com>
2020-06-30 11:51:58 -0300

    fix: identation

--- backend/src/Errors.ts
+++ backend/src/Errors.ts
@@ -1,30 +1,30 @@
 export namespace Errors {
     export class BaseError extends Error {
-	public statusCode: number;
+		public statusCode: number;
     
-	constructor(message: string) {
-	 super(message);
-	}
+		constructor(message: string) {
+			super(message);
+		}
     }
 
     export class NotFound extends BaseError {
 	constructor(){
-	super('Not found');
-	    this.statusCode = 404;
-	}
+		super('Not found');
+			this.statusCode = 404;
+		}
     }
 
     export class Forbidden extends BaseError {
-	constructor(){
-	    super("You don't have permission to acess this resource");
-	    this.statusCode = 403;
-	}
+		constructor(){
+			super("You don't have permission to acess this resource");
+			this.statusCode = 403;
+		}
     }
 
     export class Unauthorized extends BaseError {
-	constructor(){
-	    super("You aren’t authenticated, please login or send your token");
-	    this.statusCode = 401;
-	}
+		constructor(){
+			super("You aren’t authenticated, please login or send your token");
+			this.statusCode = 401;
+		}
     }
 }

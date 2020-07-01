eb5aecd7926db48aa88065ffba7cd4ac69aa89f3
SupNL <gustavoonishinl@gmail.com>
2020-07-01 13:39:00 -0300

    add: validations for routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -36,31 +36,42 @@ export default class Routes {
 
     public defineRoutes() {
         // USUARIOS
-        const userSchema = celebrate({
-            body: Joi.object().keys({ 
-                username: Joi.string().required().regex(/^[a-z0-9_]{4,20}$/),
-                password: Joi.string().required().regex(/^[a-zA-Z0-9!@#$%&*]{6,30}$/),
-                email: Joi.string().required().email(),
-                name: Joi.string().required().regex(/^[a-zA-Z ]{4,50}$/),
-                type: Joi.number().required().min(0).max(3),
-                identifierType: Joi.string().default('cpf'),
-                identifier: Joi.when('identifierType', { is: Joi.string().regex(/^cpf$/), then: Joi.document().cpf()})
-            })
-        }, {
-            abortEarly: false
-        });
-
         this.routes.get("/users", 
             this.userController.getAll.bind(this.userController));
-        this.routes.get("/users/:id", 
+        this.routes.get("/users/:id",
+            verifyAuthentication,
             this.userController.getByPk.bind(this.userController));
         this.routes.post("/users",
             verifyAuthentication, 
-            userSchema,
+            celebrate({
+                body: Joi.object().keys({ 
+                    username: Joi.string().required().regex(/^[a-z0-9_]{4,20}$/),
+                    password: Joi.string().required().regex(/^[a-zA-Z0-9!@#$%&*]{6,30}$/),
+                    email: Joi.string().required().email(),
+                    name: Joi.string().required().regex(/^[a-zA-Z ]{4,50}$/),
+                    type: Joi.number().required().min(0).max(3),
+                    identifierType: Joi.string().default('cpf'),
+                    identifier: Joi.when('identifierType', { is: Joi.string().regex(/^cpf$/), then: Joi.document().cpf()})
+                })
+            }, {
+                abortEarly: false
+            }),
             this.userController.create.bind(this.userController));
         this.routes.put("/users/:id", 
             ensureAuthentication, 
-            userSchema,
+            celebrate({
+                body: Joi.object().keys({ 
+                    username: Joi.string().regex(/^[a-z0-9_]{4,20}$/),
+                    password: Joi.string().regex(/^[a-zA-Z0-9!@#$%&*]{6,30}$/),
+                    email: Joi.string().email(),
+                    name: Joi.string().regex(/^[a-zA-Z ]{4,50}$/),
+                    type: Joi.number().min(0).max(3),
+                    identifierType: Joi.string(),
+                    identifier: Joi.when('identifierType', { is: Joi.string().regex(/^cpf$/), then: Joi.document().cpf()})
+                })
+            }, {
+                abortEarly: false
+            }),
             this.userController.edit.bind(this.userController));
         this.routes.delete("/users/:id", 
             ensureAuthentication, 
@@ -69,15 +80,59 @@ export default class Routes {
         // TAG
         this.routes.get("/tags", this.tagController.getAll.bind(this.tagController));
         this.routes.get("/tags/:id", this.tagController.getByPk.bind(this.tagController));
-        this.routes.post("/tags", ensureAuthentication, this.tagController.create.bind(this.tagController));
-        this.routes.put("/tags/:id", ensureAuthentication, this.tagController.edit.bind(this.tagController));
+        this.routes.post("/tags", 
+            ensureAuthentication, 
+            celebrate({
+                body: Joi.object().keys({ 
+                    title: Joi.string().regex(/^[a-z_]{2,30}$/).required(),
+                    description: Joi.string().min(2).max(50).required(),
+                })
+            }, {
+                abortEarly: false
+            }),
+            this.tagController.create.bind(this.tagController));
+        this.routes.put("/tags/:id", 
+            ensureAuthentication, 
+            celebrate({
+                body: Joi.object().keys({ 
+                    title: Joi.string().regex(/^[a-z_]{2,30}$/),
+                    description: Joi.string().min(2).max(50),
+                })
+            }, {
+                abortEarly: false
+            }),
+            this.tagController.edit.bind(this.tagController));
         this.routes.delete("/tags/:id", ensureAuthentication, this.tagController.delete.bind(this.tagController));
 
         // NOTICIAS
         this.routes.get("/notices", this.noticeController.getAll.bind(this.noticeController));
         this.routes.get("/notices/:id",this.noticeController.getByPk.bind(this.noticeController));
-        this.routes.post("/notices", ensureAuthentication, this.noticeController.create.bind(this.noticeController));
-        this.routes.put("/notices/:id", ensureAuthentication, this.noticeController.edit.bind(this.noticeController));
+        this.routes.post("/notices", 
+            ensureAuthentication, 
+            celebrate({
+                body: Joi.object().keys({ 
+                    title: Joi.string().min(5).max(70).required(),
+                    abstract: Joi.string().min(5).max(120).required(),
+                    text: Joi.string().required(),
+                    tags: Joi.array().items({ id: Joi.number() }),
+                })
+            }, {
+                abortEarly: false
+            }),
+            this.noticeController.create.bind(this.noticeController));
+        this.routes.put("/notices/:id", 
+            ensureAuthentication, 
+            celebrate({
+                body: Joi.object().keys({ 
+                    title: Joi.string().min(5).max(70),
+                    abstract: Joi.string().min(5).max(120),
+                    text: Joi.string(),
+                    tags: Joi.array().items({ id: Joi.number() }),
+                })
+            }, {
+                abortEarly: false
+            }),
+            this.noticeController.edit.bind(this.noticeController));
         this.routes.delete("/notices/:id", ensureAuthentication, this.noticeController.delete.bind(this.noticeController));
 
         // COMENTARIOS

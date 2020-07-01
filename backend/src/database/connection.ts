255e792490e14cfd291a376233489ea54683cc83
SupNL <gustavoonishinl@gmail.com>
2020-07-01 10:32:54 -0300

    update: access database_url from env

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,6 +1,20 @@
 import 'reflect-metadata';
-import { createConnection, Connection } from 'typeorm';
+import { createConnection, Connection, ConnectionOptions } from 'typeorm';
+require('dotenv').config();
 
-const connection: Promise<Connection> = createConnection();
+const connection: Promise<Connection> = createConnection(<ConnectionOptions>{
+    "type": "postgres",
+    "url": process.env.DATABASE_URL,
+    "synchronize": false,
+    "logging": ["query", "error"],
+    "entities": ["src/model/**/*.ts"],
+    "migrations": ["src/migration/**/*.ts"],
+    "subscribers": ["src/subscriber/**/*.ts"],
+    "cli": {
+       "entitiesDir": "src/model",
+       "migrationsDir": "src/migration",
+       "subscribersDir": "src/subscriber"
+    }
+ });
 
 export default connection;

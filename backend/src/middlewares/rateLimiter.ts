f6b69fd80c0268cfe85764d13d6488732e9ba58e
leandrosouzaa <leandror.dev@gmail.com>
2020-07-03 11:42:24 -0300

    fix rate limiter

--- backend/src/middlewares/rateLimiter.ts
+++ backend/src/middlewares/rateLimiter.ts
@@ -1,8 +1,8 @@
-const rateLimit = require("express-rate-limit");
+import * as rateLimit from 'express-rate-limit';
 
 const limiter = rateLimit({
    windowMs: 1 * 60 * 1000, 
-   max: 5,
+   max: 10,
    message: {error: 'To many requests'}
 })
 

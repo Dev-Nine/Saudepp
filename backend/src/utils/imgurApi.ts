64e382592bc9bf7727df399d1959d8149d268705
SupNL <gustavoonishinl@gmail.com>
2020-07-09 15:03:23 -0300

    add: config

--- backend/src/utils/imgurApi.ts
+++ backend/src/utils/imgurApi.ts
@@ -1,7 +1,15 @@
 import axios from 'axios';
+require('dotenv').config()
 
 export default axios.create({
     baseURL: 'https://api.imgur.com/3/',
+
 });
+
+export const config = {
+    headers: {
+        Authorization: `Client-ID ${process.env.IMGUR_ID}`
+    }
+}
 
 export const url = "https://i.imgur.com/";

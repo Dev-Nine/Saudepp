f9bf906974414b1588e127738f63dd7a29d7e863
phaalonso <phaalonso@gmail.com>
2020-07-10 15:42:37 -0300

    Refactoring Comment

--- backend/src/controller/CommentController.ts
+++ backend/src/controller/CommentController.ts
@@ -1,138 +1,129 @@
-import { Repository, getConnection, DeleteResult } from "typeorm";
+import { getRepository, DeleteResult } from "typeorm";
 import { Request, Response } from 'express';
 import { Comment } from '../model/Comment';
 import { User, UserRole } from '../model/User';
 import { Notice } from '../model/Notice';
-import { validate } from 'class-validator';
 
 import { NotFound } from '../Errors';
 
-export default class CommentController {
-    private repository : Repository<Comment>;
+// qualquer um pode realizar um comentario
+export async function validateCreate(req : Request): Promise<number>{ return 200 }
 
-    constructor(){
-        this.repository = getConnection().getRepository(Comment);
-    }
+export async function validateEdit(req : Request): Promise<number>{
+	// somente o proprio usuario pode alterar seu comentario
+	const editedComment : Comment = await getRepository(Comment).findOne(req.params["id"]);
+	if(parseInt(req.user.id) == editedComment.user.id)
+		return 200;
 
-    // qualquer um pode realizar um comentario
-    public async validateCreate(req : Request): Promise<number>{ return 200 }
+	return 403;
+}
 
-    public async validateEdit(req : Request): Promise<number>{
-        // somente o proprio usuario pode alterar seu comentario
-        const editedComment : Comment = await this.repository.findOne(req.params["id"]);
-        if(parseInt(req.user.id) == editedComment.user.id)
-            return 200;
+export async function validateDelete(req : Request): Promise<number>{
+	// adm e mod pode excluir qualquer comentario
+	if(req.user.type == UserRole.ADMIN /*|| req.user.type == UserRole.MODERADOR*/)
+		return 200;
 
-        return 403;
-    }
+	const editedComment : Comment = await getRepository(Comment).findOne(req.params["id"]);
+	if(parseInt(req.user.id) == editedComment.user.id)
+		return 200;
 
-    public async validateDelete(req : Request): Promise<number>{
-        // adm e mod pode excluir qualquer comentario
-        if(req.user.type == UserRole.ADMIN /*|| req.user.type == UserRole.MODERADOR*/)
-            return 200;
+	return 403;
+}
 
-        const editedComment : Comment = await this.repository.findOne(req.params["id"]);
-        if(parseInt(req.user.id) == editedComment.user.id)
-            return 200;
-
-        return 403;
-    }
-
-    public async getAll(req : Request, res : Response, next) : Promise<Response> {
-        const comments = await this.repository.find();
-        if(comments && comments.length > 0)
-            return res.json(comments);
-        
-		const err = new NotFound;
-        return next(err);
-    }
-
-    public async getByPk(req : Request, res : Response, next) : Promise<Response> {
-        const comment = await this.repository.findOne(req.params["id"]);
-        if(comment)
-            return res.json(comment);
+export async function getAll(req : Request, res : Response, next) : Promise<Response> {
+	const comments = await getRepository(Comment).find();
+	if(comments && comments.length > 0)
+		return res.json(comments);
 	
-		const err = new NotFound;
-        return next(err);
-    }
+	const err = new NotFound;
+	return next(err);
+}
 
-    public async processCreateData(req : Request): Promise<Comment> {
-        const comment = new Comment();
-        comment.user = new User();
-        comment.notice = new Notice();
-        comment.date = new Date;
-        const body = req["body"];
+export async function getByPk(req : Request, res : Response, next) : Promise<Response> {
+	const comment = await getRepository(Comment).findOne(req.params["id"]);
+	if(comment)
+		return res.json(comment);
 
-        comment.user.id = parseInt(req.user.id);
-        comment.notice.id = body.notice_id;
-        comment.content = body.content;
+	const err = new NotFound;
+	return next(err);
+}
 
-        return comment;
-    }
+export async function processCreateData(req : Request): Promise<Comment> {
+	const comment = new Comment();
+	comment.user = new User();
+	comment.notice = new Notice();
+	comment.date = new Date;
+	const body = req["body"];
 
-    public async processEditData(req : Request): Promise<Comment> {
-        const comment = new Comment();
-        comment.user = new User();
-        comment.notice = new Notice();
-        const body = req["body"];
+	comment.user.id = parseInt(req.user.id);
+	comment.notice.id = body.notice_id;
+	comment.content = body.content;
 
-        comment.notice.id = body.notice_id;
-        comment.content = body.content;
+	return comment;
+}
 
-        return comment;
-    }
+export async function processEditData(req : Request): Promise<Comment> {
+	const comment = new Comment();
+	comment.user = new User();
+	comment.notice = new Notice();
+	const body = req["body"];
 
-    public async create(req : Request, res : Response, next) : Promise<Response>{
-        try{
-            const statusCode = await this.validateCreate(req);
+	comment.notice.id = body.notice_id;
+	comment.content = body.content;
 
-            if(statusCode != 200)
-                return res.status(statusCode).send();
+	return comment;
+}
 
-            const comment: Comment = await this.processCreateData(req);
+export async function create(req : Request, res : Response, next) : Promise<Response>{
+	try{
+		const statusCode = await this.validateCreate(req);
 
-            const result: Comment[] = await this.repository.save([comment]);
-            return res.json(result);
-        }catch(err){
-            return next(err);
-        }
-    }
+		if(statusCode != 200)
+			return res.status(statusCode).send();
 
-    public async edit(req : Request, res : Response, next): Promise<Response> {
-        try{
-            const statusCode = await this.validateEdit(req);
-            if(statusCode != 200)
-                return res.status(statusCode).send();
+		const comment: Comment = await this.processCreateData(req);
 
-            const comment: Comment = await this.processEditData(req);
+		const result: Comment[] = await getRepository(Comment).save([comment]);
+		return res.json(result);
+	}catch(err){
+		return next(err);
+	}
+}
 
-            const foundComment = await this.repository.findOne(req.params["id"]);
-            if (foundComment) {
-				this.repository.merge(foundComment, comment);
+export async function edit(req : Request, res : Response, next): Promise<Response> {
+	try{
+		const statusCode = await this.validateEdit(req);
+		if(statusCode != 200)
+			return res.status(statusCode).send();
 
-				const result = await this.repository.save(foundComment);
+		const comment: Comment = await this.processEditData(req);
 
-				return res.json(result);
-			}
-			throw new NotFound;
-        }catch(err){
-            return next();
-        }
-    }
+		const foundComment = await getRepository(Comment).findOne(req.params["id"]);
+		if (foundComment) {
+			getRepository(Comment).merge(foundComment, comment);
 
-    public async delete(req : Request, res : Response, next): Promise<Response> {
-        try{
-            const statusCode = await this.validateDelete(req);
-            if(statusCode != 200)
-                return res.status(statusCode).send();
+			const result = await getRepository(Comment).save(foundComment);
 
-            const result: DeleteResult = await this.repository.delete(req.params["id"]);
-            if(result.affected >= 1)
-                return res.status(200).send();
-            else
-                return res.status(404).send();
-        }catch(err){
-            return next(err);
-        }
-    }
+			return res.json(result);
+		}
+		throw new NotFound;
+	}catch(err){
+		return next();
+	}
+}
+
+export async function remove(req : Request, res : Response, next): Promise<Response> {
+	try{
+		const statusCode = await this.validateDelete(req);
+		if(statusCode != 200)
+			return res.status(statusCode).send();
+
+		const result: DeleteResult = await getRepository(Comment).delete(req.params["id"]);
+		if(result.affected >= 1)
+			return res.status(200).send();
+		else
+			return res.status(404).send();
+	}catch(err){
+		return next(err);
+	}
 }

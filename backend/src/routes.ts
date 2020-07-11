8d58e4365b46516f4fe91c239329282e287589c8
Gustavo Patara <gustavoonishinl@gmail.com>
2020-07-11 15:53:11 -0300

    add: validation for user avatar

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -38,6 +38,9 @@ routes.post("/users",
 			email: Joi.string().required().email(),
 			name: Joi.string().required().regex(/^[a-zá-ùA-ZÁ-Ù ]{4,50}$/),
 			type: Joi.number().required().min(0).max(3),
+			imageId: Joi.string().max(8).optional(),
+			imageType: Joi.when('imageId', { is: Joi.exist(), then: Joi.string().max(5).required(), otherwise: Joi.string().default(null)}),
+			deleteHash: Joi.when('imageId', { is: Joi.exist(), then: Joi.string().max(16).required(), otherwise: Joi.string().default(null)}),
 			identifierType: Joi.string().default('cpf'),
 			identifier: Joi.when('identifierType', { is: Joi.string().regex(/^cpf$/), then: Joi.document().cpf()})
 		})
@@ -54,6 +57,9 @@ routes.put("/users/:id",
 			email: Joi.string().email(),
 			name: Joi.string().regex(/^[a-zá-ùA-ZÁ-Ù ]{4,50}$/),
 			type: Joi.number().min(0).max(3),
+			imageId: Joi.string().max(8).optional(),
+			imageType: Joi.when('imageId', { is: Joi.exist(), then: Joi.string().max(5).required(), otherwise: Joi.string().default(null)}),
+			deleteHash: Joi.when('imageId', { is: Joi.exist(), then: Joi.string().max(16).required(), otherwise: Joi.string().default(null)}),
 			identifierType: Joi.string(),
 			identifier: Joi.when('identifierType', { is: Joi.string().regex(/^cpf$/), then: Joi.document().cpf()})
 		})
@@ -101,9 +107,9 @@ routes.post("/notices",
 			abstract: Joi.string().min(5).max(120).required(),
 			text: Joi.string().required(),
 			tags: Joi.array().items({ id: Joi.number() }),
-			imageId: Joi.string().max(8).optional(),
-			imageType: Joi.when('imageId', { is: Joi.exist(), then: Joi.string().max(5).required(), otherwise: Joi.string().default(null)}),
-			deleteHash: Joi.when('imageId', { is: Joi.exist(), then: Joi.string().max(16).required(), otherwise: Joi.string().default(null)}),
+			imageId: Joi.string().max(8).optional().allow(null).default(null),
+			imageType: Joi.when('imageId', { is: Joi.exist().not(null), then: Joi.string().max(5).required(), otherwise: Joi.string().default(null)}),
+			deleteHash: Joi.when('imageId', { is: Joi.exist().not(null), then: Joi.string().max(16).required(), otherwise: Joi.string().default(null)}),
 		})
 	}, {
 		abortEarly: false

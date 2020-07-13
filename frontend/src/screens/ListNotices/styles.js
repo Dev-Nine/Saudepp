d0dd61943954de26804f407626175cdb5a7eab2c
Gustavo Patara <gustavoonishinl@gmail.com>
2020-07-13 15:33:35 -0300

    fix: width

--- frontend/src/screens/ListNotices/styles.js
+++ frontend/src/screens/ListNotices/styles.js
@@ -66,6 +66,7 @@ export const ContainerNoticia = styled.div`
    border-radius: 10px;
    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
    margin-bottom: 20px;
+   width: 100%;
 
    h1 {
       font-size: 30px;

620df7e3137f22c6c6f9af511ca4c6e75581a336
phaalonso <phaalonso@gmail.com>
2020-07-14 18:33:24 -0300

    Removing class validade from worker covid

--- backend/src/services/workerCovidInfo.ts
+++ backend/src/services/workerCovidInfo.ts
@@ -2,7 +2,6 @@ import { getConnection } from 'typeorm';
 
 import api from '../utils/api';
 import { CovidInfo } from '../model/CovidInfo';
-import { validate } from 'class-validator';
 
 interface covidApiData {
     data : {
@@ -15,40 +14,36 @@ interface covidApiData {
 
 export default async function workerCovidInfo () {
     try {
-	const response = await api.get('api/report/v1/brazil');
-	const data : covidApiData = response.data;
+		const response = await api.get('api/report/v1/brazil');
+		const data : covidApiData = response.data;
 
-	const { confirmed, deaths, recovered, updated_at } = data.data; // why
+		const { confirmed, deaths, recovered, updated_at } = data.data; // why
 
-	const covid = new CovidInfo();
-	covid.confirmed = confirmed;
-	covid.recovered = recovered;
-	covid.deaths = deaths;
-	covid.lethality = String(100 * (deaths / confirmed));
-	covid.date = new Date(updated_at);
+		if (!(confirmed && deaths && recovered && updated_at))
+			throw 'Error in geting data from API';
 
-	// console.log(covid);
+		const covid = new CovidInfo();
+		covid.confirmed = confirmed;
+		covid.recovered = recovered;
+		covid.deaths = deaths;
+		covid.lethality = String(100 * (deaths / confirmed));
+		covid.date = new Date(updated_at);
 
-	const errors = await validate(covid);
-
-	if (errors.length > 0) {
-	    console.error(errors);
-	} else {
 	    await getConnection().transaction(async manager => {
-		const datas = await manager.getRepository(CovidInfo).find({
-		    order:{
-			id : "DESC"
-		    }
-		});
-		if (datas.length == 0 || datas[0].date.getTime() !== covid.date.getTime()){
-		    await manager.getRepository(CovidInfo).save(covid);
-		}
+			const datas = await manager.getRepository(CovidInfo).find({
+				order:{
+				id : "DESC"
+				}
+			});
+			if (datas.length == 0 || datas[0].date.getTime() !== covid.date.getTime()){
+				await manager.getRepository(CovidInfo).save(covid);
+			}
 	    });
-	}
+		
     } catch (err) {
-	console.error('Error in request to Covid Api');
-	if (process.env.DEV) {
-	    console.error(err);
+		console.error('Error in request to Covid Api');
+		if (process.env.DEV) {
+			console.error(err);
+		}
 	}
-    }
 }

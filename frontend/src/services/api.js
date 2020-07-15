b9bfb2ce685799e793148d16671f040a1c9b40bc
phaalonso <phaalonso@gmail.com>
2020-07-15 14:39:25 -0300

    Changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
+import useSWR from 'swr';
 
 const api = axios.create({
    baseURL: 'http://localhost:3333',
 });
+
+export function useAxios(url) {
+   const { data, error } = useSWR(url, async (link) => {
+      const response = await api.get(link);
+
+      return response.data;
+   });
+   return { data, error };
+}
 
 export default api;

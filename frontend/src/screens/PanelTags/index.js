a083e0899def0a0133a22a9f3ec487745bf94370
leandrosouzaa <leandror.dev@gmail.com>
2020-07-16 16:13:17 -0300

    fetch tags from api

--- frontend/src/screens/PanelTags/index.js
+++ frontend/src/screens/PanelTags/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { FiSearch, FiX } from 'react-icons/fi';
 import { Container, Table, TableHeader, TableLine, Button } from './styles';
@@ -9,6 +9,18 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 export default function PanelTags() {
+   const [tags, setTags] = useState([]);
+
+   useEffect(() => {
+      async function loadTags() {
+         const { data } = await api.get('/tags');
+
+         setTags(data);
+      }
+
+      loadTags();
+   }, []);
+
    return (
       <>
          <Header />
@@ -21,17 +33,20 @@ export default function PanelTags() {
                      <div>Descrição</div>
                      <div>+</div>
                   </TableHeader>
-                  <TableLine>
-                     <div>Covid 19</div>
-                     <div>
-                        <Button>
-                           <FiSearch />
-                        </Button>
-                        <Button isDelete>
-                           <FiX />
-                        </Button>
-                     </div>
-                  </TableLine>
+
+                  {tags.map((t) => (
+                     <TableLine key={t.id}>
+                        <div>{t.description}</div>
+                        <div>
+                           <Button>
+                              <FiSearch />
+                           </Button>
+                           <Button isDelete>
+                              <FiX />
+                           </Button>
+                        </div>
+                     </TableLine>
+                  ))}
                </Table>
             </Container>
          </div>

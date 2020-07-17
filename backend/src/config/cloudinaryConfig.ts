67a280c3bffaa3d6e083204d17f4fff78736db24
Gustavo Patara <gustavoonishinl@gmail.com>
2020-07-17 10:48:30 -0300

    add: cloudinary config

---
+++ backend/src/config/cloudinaryConfig.ts
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const config = {
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+}
+
+export default config;

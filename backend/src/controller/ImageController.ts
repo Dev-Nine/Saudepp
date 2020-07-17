d562d7cd1db883ddc3589a4a2010dcdadd479fed
Gustavo Patara <gustavoonishinl@gmail.com>
2020-07-17 10:49:12 -0300

    update: new image hosting api

--- backend/src/controller/ImageController.ts
+++ backend/src/controller/ImageController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
+import cloudinary from 'cloudinary'
 import fs from 'fs';
-import imgurApi, {config} from '../utils/imgurApi';
+import cloudinaryConfig from '../config/cloudinaryConfig';
 
 export async function create(req : Request, res : Response, next) : Promise<Response>{
 	const {path} = req.file;
@@ -8,22 +9,23 @@ export async function create(req : Request, res : Response, next) : Promise<Resp
 		fs.readFile(path, (err, data) => {
 			const base64image = Buffer.from(data).toString('base64')
 			var form = {
-				type: 'base64',
+				type: `image/${path.split('.')[path.split('.').length - 1]}`,
 				image: base64image
 			}
 
-			imgurApi.post("upload/", form, config)
-			.then(response => {
-				const result = response.data
-				const data = {
-					id: result.data.id,
-					type: result.data.type.split('/')[1],
-					deletehash: result.data.deletehash
-				}
-				return res.status(result.status).json(data)
-			}).catch(err => {
-				return next(err);
-			})
+			cloudinary.v2.config(cloudinaryConfig);
+
+			cloudinary.v2.uploader.upload(
+				`data:${form.type};base64,${form.image}`,
+				(err, result) => {
+					if(err)
+						throw new Error;
+					const data = {
+						imageId: result.public_id,
+						imageType: result.format
+					}
+					return res.status(200).json(data);
+				})
 		});
 	}catch(err){
 		return next(err);

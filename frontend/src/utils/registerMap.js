e7c5ff22cc69271fb77353990d668fc06040a99c
Gustavo Patara <gustavoonishinl@gmail.com>
2020-07-21 13:32:28 -0300

    add: register stuff

---
+++ frontend/src/utils/registerMap.js
@@ -0,0 +1,106 @@
+import * as Yup from 'yup';
+import { cpf } from 'cpf-cnpj-validator';
+
+export default function setMap() {
+   const registerMap = new Map();
+
+   registerMap.set('crp', {
+      name: 'Psicólogo',
+      register: 'CRP',
+      state: true,
+      mask: '99/999999',
+      test: Yup.string().matches(
+         /^[1-9/]{9}$/,
+         'Um CRP deve possuir 8 digitos numéricos',
+      ),
+   });
+
+   registerMap.set('crf', {
+      name: 'Farmacêutico / Bioquímico',
+      register: 'CRF',
+      state: false,
+      mask: '99999',
+      test: Yup.string().matches(
+         /^[1-9]{5}$/,
+         'Um CRF deve possuir 5 digitos numéricos',
+      ),
+   });
+
+   registerMap.set('crfa', {
+      name: 'Fonoaudióloga',
+      register: 'CRFa',
+      state: false,
+      mask: '99.999',
+      test: Yup.string().matches(
+         /^[1-9.]{6}$/,
+         'Um CRFa deve possuir 5 digitos numéricos',
+      ),
+   });
+
+   registerMap.set('cro', {
+      name: 'Dentista',
+      register: 'CRO',
+      state: true,
+      mask: '99999',
+      test: Yup.string().matches(
+         /^[1-9]{5}$/,
+         'Um CRO deve possuir 5 digitos numéricos',
+      ),
+   });
+
+   registerMap.set('coren', {
+      name: 'Enfermeiro',
+      register: 'Coren',
+      state: false,
+      mask: '999999',
+      test: Yup.string().matches(
+         /^[1-9]{6}$/,
+         'Um Coren deve possuir 6 digitos numéricos',
+      ),
+   });
+
+   registerMap.set('crm', {
+      name: 'Médico',
+      register: 'CRM',
+      state: false,
+      mask: '999999',
+      test: Yup.string().matches(
+         /^[1-9]{6}$/,
+         'Um CRM deve possuir 6 digitos numéricos',
+      ),
+   });
+
+   registerMap.set('acm', {
+      name: 'Agente Comunitário de Endemia',
+      register: 'Número de matrícula (ACM)',
+      state: false,
+      mask: '999999',
+      test: Yup.string().matches(
+         /^[1-9]{6}$/,
+         'O número da matrícula deve possuir 6 digitos numéricos',
+      ),
+   });
+
+   registerMap.set('ace', {
+      name: 'Agente de Combate à Endemia',
+      register: 'Número de matrícula (ACE)',
+      state: false,
+      mask: '999999',
+      test: Yup.string().matches(
+         /^[1-9]{6}$/,
+         'O número da matrícula deve possuir 6 digitos numéricos',
+      ),
+   });
+
+   registerMap.set('cpf', {
+      name: 'Outros',
+      register: 'CPF',
+      state: false,
+      mask: '999.999.999-99',
+      test: Yup.string().test('CPF válido', 'Informe um CPF válido', (value) =>
+         cpf.isValid(value),
+      ),
+   });
+
+   return registerMap;
+}

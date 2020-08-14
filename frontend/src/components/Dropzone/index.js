49b763ae1e38a8f4ce79b6e977f08afe7b6bb9fd
Gustavo Patara <gustavoonishinl@gmail.com>
2020-08-14 13:10:12 -0300

    add: dropzone component

---
+++ frontend/src/components/Dropzone/index.js
@@ -0,0 +1,30 @@
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { Container } from './styles';
+
+const Dropzone = ({ setFile }) => {
+    const onDrop = useCallback(
+        (files) => {
+            const file = files[0];
+            setFile(file);
+        },
+        [setFile],
+    );
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+        accept: 'image/jpeg, image/png',
+        multiple: false,
+    });
+    return (
+        <Container {...getRootProps()}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+                <p>Solte a foto!</p>
+            ) : (
+                <p>Selecione ou arraste a foto aqui</p>
+            )}
+        </Container>
+    );
+};
+
+export default Dropzone;
---
+++ frontend/src/components/Dropzone/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: white;
+    height: 100px;
+    border: 2px dashed var(--main-light-color);
+`;

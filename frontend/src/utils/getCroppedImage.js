59861dee03f752eb5d9c216420a862f90dee1545
Gustavo Patara <gustavoonishinl@gmail.com>
2020-08-21 17:47:46 -0300

    add: util to return cropped image (update later)

---
+++ frontend/src/utils/getCroppedImage.js
@@ -0,0 +1,32 @@
+export default async function getCroppedImg(image, crop, fileName) {
+    const canvas = document.createElement('canvas');
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(
+        image,
+        crop.x * scaleX,
+        crop.y * scaleY,
+        crop.width * scaleX,
+        crop.height * scaleY,
+        0,
+        0,
+        crop.width,
+        crop.height,
+    );
+
+    // As a blob
+    return new Promise((resolve) => {
+        canvas.toBlob(
+            (blob) => {
+                blob.name = fileName;
+                resolve(blob);
+            },
+            'image/jpeg',
+            1,
+        );
+    });
+}

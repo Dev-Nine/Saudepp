6a3319c8b3ccd2c362e9b6270f65c9ca38131831
Gustavo Patara <gustavoonishinl@gmail.com>
2020-10-19 09:35:26 -0300

    update: ident

--- frontend/src/screens/NotFound/index.js
+++ frontend/src/screens/NotFound/index.js
@@ -4,11 +4,11 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 export default function NotFound() {
-   return (
-      <>
-         <Header />
-         <h1 style={{ padding: 20 }}> 404 - Page not found </h1>
-         <Footer />
-      </>
-   );
+    return (
+        <>
+            <Header />
+            <h1 style={{ padding: 20 }}> 404 - Page not found </h1>
+            <Footer />
+        </>
+    );
 }

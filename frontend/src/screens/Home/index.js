6a840c0f236b0bdd9fd1d3451ad1c1d3c0e3aee8
Gustavo Patara <gustavoonishinl@gmail.com>
2020-10-19 11:41:25 -0300

    fix: return null when notices aren't found

--- frontend/src/screens/Home/index.js
+++ frontend/src/screens/Home/index.js
@@ -11,39 +11,38 @@ import CoronaCard from '../../components/CoronaCard';
 import Footer from '../../components/Footer';
 
 export default function Home() {
-   const { data: notices } = useAxios('/notices?page=0&limit=4');
+    const { data: notices } = useAxios('/notices?page=0&limit=4');
 
-   useEffect(() => {
-      document.title = 'Home';
-   }, []);
+    useEffect(() => {
+        document.title = 'Home';
+    }, []);
 
-   return (
-      <>
-         <Header />
+    return (
+        <>
+            <Header />
 
-         <div className="main">
-            <CoronaCard />
-            <Sobre>
-               <p>
-                  &emsp;A plataforma digital denominada Saúde++, visa atualizar
-                  e orientar os cidadões do município de Presidente Epitácio,
-                  trazendo dados e fatos verídicos em relação a pandemia do
-                  Coronavírus e entre outras doenças que continuam presentes em
-                  nossa sociedade, como a Dengue, Sarampo, entre outras.
-               </p>
-               <Link to="/about">Saiba mais</Link>
-            </Sobre>
+            <div className="main">
+                <CoronaCard />
+                <Sobre>
+                    <p>
+                        &emsp;A plataforma digital denominada Saúde++, visa
+                        atualizar e orientar os cidadões do município de
+                        Presidente Epitácio, trazendo dados e fatos verídicos em
+                        relação a pandemia do Coronavírus e entre outras doenças
+                        que continuam presentes em nossa sociedade, como a
+                        Dengue, Sarampo, entre outras.
+                    </p>
+                    <Link to="/about">Saiba mais</Link>
+                </Sobre>
 
-            <div>
-               {notices ? (
-                  <CardGroup title="Novas noticias" data={notices} />
-               ) : (
-                  <p>Carregando</p>
-               )}
+                <div>
+                    {notices ? (
+                        <CardGroup title="Novas noticias" data={notices} />
+                    ) : null}
+                </div>
             </div>
-         </div>
 
-         <Footer />
-      </>
-   );
+            <Footer />
+        </>
+    );
 }

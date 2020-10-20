e57f7480611b9ad28e913df9f8d33ae4f74d9b56
Gustavo Patara <gustavoonishinl@gmail.com>
2020-10-20 13:40:01 -0300

    refactor: indent

--- frontend/src/components/Input/index.js
+++ frontend/src/components/Input/index.js
@@ -6,44 +6,48 @@ import InputMask from 'react-input-mask';
 import { Container, Error } from './styles';
 
 const Input = ({ name, icon: Icon, ...rest }) => {
-   const inputRef = useRef(null);
-   const [isFocused, setIsFocused] = useState(false);
-   const [isFilled, setIsFilled] = useState(false);
-   const { fieldName, error, registerField } = useField(name);
+    const inputRef = useRef(null);
+    const [isFocused, setIsFocused] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
+    const { fieldName, error, registerField } = useField(name);
 
-   const handleInputBlur = useCallback(() => {
-      setIsFocused(false);
+    const handleInputBlur = useCallback(() => {
+        setIsFocused(false);
 
-      setIsFilled(!!inputRef.current.value);
-   }, []);
+        setIsFilled(!!inputRef.current.value);
+    }, []);
 
-   const handleInputFocus = useCallback(() => setIsFocused(true), []);
+    const handleInputFocus = useCallback(() => setIsFocused(true), []);
 
-   useEffect(() => {
-      registerField({
-         name: fieldName,
-         ref: inputRef.current,
-         path: 'value',
-      });
-   }, [fieldName, registerField]);
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            ref: inputRef.current,
+            path: 'value',
+        });
+    }, [fieldName, registerField]);
 
-   return (
-      <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
-         {Icon && <Icon size={20} />}
-         <InputMask
-            onFocus={() => handleInputFocus()}
-            onBlur={() => handleInputBlur()}
-            ref={inputRef}
-            {...rest}
-         />
+    return (
+        <Container
+            isErrored={!!error}
+            isFilled={isFilled}
+            isFocused={isFocused}
+        >
+            {Icon && <Icon size={20} />}
+            <InputMask
+                onFocus={() => handleInputFocus()}
+                onBlur={() => handleInputBlur()}
+                ref={inputRef}
+                {...rest}
+            />
 
-         {error && (
-            <Error title={error}>
-               <FiAlertCircle color="#ff0000" size={20} />
-            </Error>
-         )}
-      </Container>
-   );
+            {error && (
+                <Error title={error}>
+                    <FiAlertCircle color="#ff0000" size={20} />
+                </Error>
+            )}
+        </Container>
+    );
 };
 
 export default Input;

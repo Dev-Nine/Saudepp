d302b6b6f0c537140aff6169f50a99ffb1407eaf
Gustavo Patara <gustavoonishinl@gmail.com>
2020-10-21 10:38:33 -0300

    add: modal style

---
+++ frontend/src/styles/ModalStyle.js
@@ -0,0 +1,13 @@
+const modalStyle = {
+    content: {
+        right: 'auto',
+        width: '70%',
+        height: '80vh',
+        left: '15%',
+        top: '10%',
+        padding: '8px',
+        position: 'absolute',
+    },
+};
+
+export default modalStyle;

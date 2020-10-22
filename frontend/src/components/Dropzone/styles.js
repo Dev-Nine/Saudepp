fa7027bb1968d097c565d04970e14f852414e307
Gustavo Patara <gustavoonishinl@gmail.com>
2020-10-22 12:29:28 -0300

    update: add padding, cursor change on hover

--- frontend/src/components/Dropzone/styles.js
+++ frontend/src/components/Dropzone/styles.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    :hover {
+        cursor: pointer;
+    }
     display: flex;
     align-items: center;
     justify-content: center;
     background: white;
     height: 100px;
     border: 2px dashed var(--main-light-color);
+    padding: 16px;
 `;

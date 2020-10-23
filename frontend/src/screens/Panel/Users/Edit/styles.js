5ff36757979ec91bc7b8570941317e321dbf9176
Gustavo Patara <gustavoonishinl@gmail.com>
2020-10-23 12:41:09 -0300

    update: add avatar style

--- frontend/src/screens/Panel/Users/Edit/styles.js
+++ frontend/src/screens/Panel/Users/Edit/styles.js
@@ -6,4 +6,18 @@ export const Container = styled.div`
     justify-content: center;
     align-items: center;
     padding: 8px;
+
+    form {
+        .avatar img {
+            border: 2px solid var(--main-light-color);
+            border-radius: 50%;
+            height: 160px;
+        }
+
+        .avatar-change {
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+        }
+    }
 `;

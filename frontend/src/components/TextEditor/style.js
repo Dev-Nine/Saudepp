bcf10e588be3be59de0e51870d6d795739669cc7
Gustavo Patara <gustavoonishinl@gmail.com>
2020-10-28 11:45:48 -0300

    add: container for editor

---
+++ frontend/src/components/TextEditor/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const EditorContainer = styled.div`
+    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+    background-color: white;
+    border-radius: 8px;
+`;

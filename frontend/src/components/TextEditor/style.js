0bc52306900654de39d6b18f550005846d4b3f96
Gustavo Patara <gustavoonishinl@gmail.com>
2020-10-30 10:28:34 -0300

    update: inside position

--- frontend/src/components/TextEditor/style.js
+++ frontend/src/components/TextEditor/style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const EditorContainer = styled.div`
     box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+    list-style-position: inside;
     background-color: white;
     border-radius: 8px;
 `;

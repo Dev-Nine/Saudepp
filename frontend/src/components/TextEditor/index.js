1e952541a8656c10a19735b3465ebdb44adb2f53
Gustavo Patara <gustavoonishinl@gmail.com>
2020-11-03 12:59:29 -0300

    update: finish style, handle images

--- frontend/src/components/TextEditor/index.js
+++ frontend/src/components/TextEditor/index.js
@@ -1,17 +1,16 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import { createEditor, Transforms, Editor } from 'slate';
-import { Editable, Slate, withReact } from 'slate-react';
+import { Editable, Slate, useSelected, useSlate, withReact } from 'slate-react';
 import { withHistory } from 'slate-history';
+import { FiBold, FiImage, FiItalic, FiList, FiUnderline } from 'react-icons/fi';
+import { useRef } from 'react';
 import { NoticeContainer } from '../../styles/NoticeStyle';
-import { EditorContainer, ButtonStyle } from './style';
+import { EditorContainer, ToolBar, ButtonStyle } from './style';
 
 const CustomEditor = {
     isMarkActive(editor, format) {
         const marks = Editor.marks(editor);
-        if (marks) {
-            return marks[format] === true;
-        }
-        return false;
+        return marks ? marks[format] === true : false;
     },
 
     toggleMark(editor, format) {
@@ -66,7 +65,7 @@ const insertImage = (editor, url) => {
         },
         {
             type: 'p',
-            children: [text],
+            children: [{ text: '' }],
         },
     ];
 
@@ -84,7 +83,7 @@ const withImages = (editor) => {
         const { files } = data;
 
         if (files && files.length > 0) {
-            for (const file of files) {
+            Array.from(files).forEach((file) => {
                 const reader = new FileReader();
                 const [mime] = file.type.split('/');
 
@@ -96,7 +95,7 @@ const withImages = (editor) => {
 
                     reader.readAsDataURL(file);
                 }
-            }
+            });
         } else {
             insertData(data);
         }
@@ -106,6 +105,7 @@ const withImages = (editor) => {
 };
 
 const TextEditor = () => {
+    const imageInputRef = useRef();
     const editor = useMemo(
         () => withImages(withHistory(withReact(createEditor()))),
         [],
@@ -118,15 +118,21 @@ const TextEditor = () => {
     ]);
 
     const ToggleButton = ({ tooltip, type, format, children }) => {
+        const editorTest = useSlate();
         return (
             <ButtonStyle
                 type="button"
+                active={
+                    type === 'mark'
+                        ? CustomEditor.isMarkActive(editorTest, format)
+                        : CustomEditor.isBlockActive(editorTest, format)
+                }
                 title={tooltip}
                 onMouseDown={(event) => {
                     event.preventDefault();
                     type === 'mark'
-                        ? CustomEditor.toggleMark(editor, format)
-                        : CustomEditor.toggleBlock(editor, format);
+                        ? CustomEditor.toggleMark(editorTest, format)
+                        : CustomEditor.toggleBlock(editorTest, format);
                 }}
             >
                 {children}
@@ -138,28 +144,53 @@ const TextEditor = () => {
         if (leaf.b) {
             children = <strong>{children}</strong>;
         }
+        if (leaf.i) {
+            children = <em>{children}</em>;
+        }
+        if (leaf.u) {
+            children = <u>{children}</u>;
+        }
 
         return <span {...attributes}>{children}</span>;
     };
 
-    const renderElement = useCallback(({ attributes, children, element }) => {
+    const ImageElement = ({ attributes, children, element }) => {
+        const selected = useSelected();
+        return (
+            <div
+                style={{ display: 'flex', justifyContent: 'center' }}
+                {...attributes}
+            >
+                <img
+                    src={element.url}
+                    alt="A"
+                    style={{
+                        outline:
+                            selected && '2px solid var(--main-basic-color)',
+                    }}
+                />
+                {children}
+            </div>
+        );
+    };
+
+    const renderElement = useCallback((props) => {
+        const { attributes, children, element } = props;
         switch (element.type) {
             case 'h1':
                 return <h1 {...attributes}>{children}</h1>;
+            case 'h2':
+                return <h2 {...attributes}>{children}</h2>;
+            case 'h3':
+                return <h3 {...attributes}>{children}</h3>;
             case 'ul':
                 return <ul {...attributes}>{children}</ul>;
+            case 'ol':
+                return <ol {...attributes}>{children}</ol>;
             case 'li':
                 return <li {...attributes}>{children}</li>;
             case 'image':
-                return (
-                    <div
-                        style={{ display: 'flex', justifyContent: 'center' }}
-                        {...attributes}
-                    >
-                        <img src={element.url} />
-                        {children}
-                    </div>
-                );
+                return <ImageElement {...props} />;
             default:
                 return <p {...attributes}>{children}</p>;
         }
@@ -169,36 +200,107 @@ const TextEditor = () => {
         return <Leaf {...props} />;
     }, []);
 
+    const handleFileUpload = (e) => {
+        const file = imageInputRef.current.files[0];
+        const reader = new FileReader();
+        const [mime] = file.type.split('/');
+
+        if (mime === 'image') {
+            reader.addEventListener('load', () => {
+                const url = reader.result;
+                insertImage(editor, url);
+            });
+
+            reader.readAsDataURL(file);
+        }
+        e.target.value = null;
+    };
+
     return (
         <Slate
             editor={editor}
             value={value}
             onChange={(newValue) => setValue(newValue)}
         >
-            <div>
+            <ToolBar>
                 <ToggleButton tooltip="Negrito" type="mark" format="b">
-                    B
+                    <FiBold />
+                </ToggleButton>
+                <ToggleButton tooltip="Itálico" type="mark" format="i">
+                    <FiItalic />
+                </ToggleButton>
+                <ToggleButton tooltip="Sublinhado" type="mark" format="u">
+                    <FiUnderline />
                 </ToggleButton>
                 <ToggleButton tooltip="Header 1" type="block" format="h1">
                     H1
                 </ToggleButton>
-                <ToggleButton tooltip="Lista" type="block" format="ul">
-                    UL
+                <ToggleButton tooltip="Header 2" type="block" format="h2">
+                    H2
                 </ToggleButton>
-            </div>
+                <ToggleButton tooltip="Header 3" type="block" format="h3">
+                    H3
+                </ToggleButton>
+                <ToggleButton
+                    tooltip="Lista não ordenada"
+                    type="block"
+                    format="ul"
+                >
+                    <FiList />
+                </ToggleButton>
+                <ToggleButton tooltip="Lista ordenada" type="block" format="ol">
+                    1.
+                </ToggleButton>
+                <input
+                    type="file"
+                    id="selectFile"
+                    accept="image/*"
+                    style={{ display: 'none' }}
+                    onChange={handleFileUpload}
+                    ref={imageInputRef}
+                />
+                <ButtonStyle
+                    type="file"
+                    title="Anexar imagem"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        imageInputRef.current.click();
+                    }}
+                >
+                    <FiImage />
+                </ButtonStyle>
+            </ToolBar>
             <EditorContainer>
                 <NoticeContainer style={{ padding: 8 }}>
                     <Editable
                         style={{
                             minHeight: 160,
                         }}
+                        placeholder="Escreva o noticiário aqui..."
                         renderElement={renderElement}
                         renderLeaf={renderLeaf}
                         onKeyDown={(e) => {
-                            // unfinished, vou arrumar dps
+                            if (e.keyCode === 13) {
+                                const fragment = editor.getFragment();
+                                if (fragment[0].type === 'image') {
+                                    Transforms.select(editor, editor.selection);
+                                    Transforms.insertNodes(editor, {
+                                        type: 'h1',
+                                        children: [{ text: '' }],
+                                    });
+                                    Transforms.delete(editor);
+                                }
+                                return;
+                            }
                             if (e.ctrlKey && e.key === 'b') {
                                 e.preventDefault();
                                 CustomEditor.toggleMark(editor, 'b');
+                            } else if (e.ctrlKey && e.key === 'i') {
+                                e.preventDefault();
+                                CustomEditor.toggleMark(editor, 'i');
+                            } else if (e.ctrlKey && e.key === 'u') {
+                                e.preventDefault();
+                                CustomEditor.toggleMark(editor, 'u');
                             }
                         }}
                     />

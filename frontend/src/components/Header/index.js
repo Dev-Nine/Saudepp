8c7df6d80494768af03dc51d57151f0cc0da9af4
Gustavo Patara <gustavoonishinl@gmail.com>
2020-11-09 11:03:14 -0300

    add: some new buttons on header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -9,49 +9,57 @@ import { Container } from './styles';
 import { useAuth } from '../../hooks/AuthProvider';
 
 function Header() {
-   const [dropdown, setDropdown] = useState('');
-   const { user, signOut } = useAuth();
+    const [dropdown, setDropdown] = useState('');
+    const { user, signOut } = useAuth();
 
-   function handleIconClick() {
-      setDropdown((prevState) => {
-         if (prevState === 'show-dropdown') return '';
-         return 'show-dropdown';
-      });
-   }
+    function handleIconClick() {
+        setDropdown((prevState) => {
+            if (prevState === 'show-dropdown') return '';
+            return 'show-dropdown';
+        });
+    }
 
-   return (
-      <>
-         <Container>
-            <div>
-               <div className="menu">
-                  <Link to="/" className="menu-logo">
-                     <p>Saúde++</p>
-                     <p>Presidente Epitácio - SP</p>
-                  </Link>
-                  <div className="icon" onClick={handleIconClick}>
-                     <FiMenu color="#0094DE" size={40} />
-                  </div>
-                  <nav className={dropdown}>
-                     <Link to="/notices">Notícias</Link>
-                     <Link to="/faq">Dúvidas frequentes</Link>
-                     {!!user && <Link to="/panel">Painel de controle</Link>}
-                     <Link to="/about">Sobre nós</Link>
-                     {!!user && (
-                        <Link
-                           to="/"
-                           className="logout"
-                           onClick={() => signOut()}
-                        >
-                           Sair
+    return (
+        <>
+            <Container>
+                <div>
+                    <div className="menu">
+                        <Link to="/" className="menu-logo">
+                            <p>Saúde++</p>
+                            <p>Presidente Epitácio - SP</p>
                         </Link>
-                     )}
-                  </nav>
-                  {user && <img src={user.imageUrl} alt="Avatar" />}
-               </div>
-            </div>
-         </Container>
-      </>
-   );
+                        <div className="icon" onClick={handleIconClick}>
+                            <FiMenu color="#0094DE" size={40} />
+                        </div>
+                        <nav className={dropdown}>
+                            <Link to="/notices">Notícias</Link>
+                            <Link to="/faq">Dúvidas frequentes</Link>
+                            {!!user && (
+                                <Link to="/panel">Painel de controle</Link>
+                            )}
+                            {!!user && (
+                                <Link to={`/panel/users/edit/${user.id}`}>
+                                    Sua conta
+                                </Link>
+                            )}
+                            <Link to="/about">Sobre nós</Link>
+                            {!!user && (
+                                <Link
+                                    to="/"
+                                    className="logout"
+                                    onClick={() => signOut()}
+                                >
+                                    Sair
+                                </Link>
+                            )}
+                            {!user && <Link to="/login">Entrar</Link>}
+                        </nav>
+                        {user && <img src={user.imageUrl} alt="Avatar" />}
+                    </div>
+                </div>
+            </Container>
+        </>
+    );
 }
 
 export default Header;
